cf3c541407fd45a5be8f002687265a15d07dad38
Steve Roma <sjroma@hotmail.com>
2017-06-25 16:15:37 -0400

    tweak to let user decide number of paragraphs to gen

--- lorem-app.js
+++ lorem-app.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
+const loremIpsum = require('lorem-ipsum');
+let number;
 
-app.get('/', function (req, res) {
-  res.send('check out localhost:3000/lorem');
-});
-
-var loremIpsum = require('lorem-ipsum'),
-    output = loremIpsum ({
-    count: 3,
+// Let the user decide how many paragraphs get generated
+app.get('/lorem/:paragraghCount', function (req, res) {
+  let output = loremIpsum ({
+    count: req.params.paragraphCount,
     units: 'paragraphs',
     format: 'html'
-    });
-
-app.get('/lorem', function (req, res) {
+  });
   res.send(output);
 });
 
+// Works but the number of paragraphs is set by the program
+//app.get('/', function (req, res) {
+//  res.send('check out localhost:3000/lorem');
+//});
+//
+//var loremIpsum = require('lorem-ipsum'),
+//    output = loremIpsum ({
+//    count: 3,
+//    units: 'paragraphs',
+//    format: 'html'
+//    });
+//
+//app.get('/lorem', function (req, res) {
+//  res.send(output);
+//});
+
 app.listen(3000, function () {
-  console.log('Successfully started lorem application')
+  console.log('Successfully started lorem-ipsom application')
 });
